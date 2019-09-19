ae871c9efbc35f83e0aedec609fd03a257b75ab1
Philip Johansson <philipjohansson@Philips-MacBook-Pro-5.local>
2019-09-19 12:52:52 +0200

    sql uppdate

--- TABELS.sql
+++ TABELS.sql
@@ -16,10 +16,10 @@ CREATE TABLE People (
 
 
 INSERT INTO People (name, inbetalat) VALUES ("Aom", 1125);
-INSERT INTO People (name, inbetalat) VALUES ("Olle", 425);
-INSERT INTO People (name, inbetalat) VALUES ("Philip J", 725);
+INSERT INTO People (name, inbetalat) VALUES ("Filip G", 300);
+INSERT INTO People (name, inbetalat) VALUES ("Philip J", 1150);
 INSERT INTO People (name, inbetalat) VALUES ("Filip L", 500);
-INSERT INTO People (name, inbetalat) VALUES ("Caser", 425);
+INSERT INTO People (name, inbetalat) VALUES ("Casper", 425);
 INSERT INTO People (name, inbetalat) VALUES ("Samuel", 425);
-INSERT INTO People (name, inbetalat) VALUES ("Hanna", 425);
-INSERT INTO People (name, inbetalat) VALUES ("Felix", 425);
+INSERT INTO People (name, inbetalat) VALUES ("Hanna", 1225);
+INSERT INTO People (name, inbetalat) VALUES ("Felix", 1025);
